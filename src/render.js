5f9fa8e4ff8b8db1d0540f6830a9e054d116970e
ClckZ <jannisurb@gmail.com>
2026-07-16 22:20:30 +0200

    Added Every Button

--- src/render.js
+++ src/render.js
@@ -1,12 +1,18 @@
-const categorize_btn = document.getElementById('categorize-btn')
+const categorize_btn = document.getElementById("categorize-btn");
+const how_many_input = document.getElementById("how_many_input");
 
+categorize_btn.addEventListener("click", categorizePosts);
 async function categorizePosts() {
-    const how_many = 10;
+  animate(how_many_input, {
+    scale: [1.3, 1],
+    duration: 400,
+    ease: "outBack",
+  });
 
-    const response = await fetch(
-    `http://127.0.0.1:8000/categorize?how_many=${how_many}`
-    );
+  const response = await fetch(
+    `http://127.0.0.1:8000/categorize?how_many=${parseInt(how_many_input.value)}`,
+  );
 
-    const data = await response.json();
-    console.log(data);
+  const data = await response.json();
+  console.log(data);
 }
